2d2ce09e000aff92fd224e83825d7e0175bd8775
junyi <47441677+immanuelhume@users.noreply.github.com>
2022-10-25 10:35:58 +0800

    Group materials into one file

--- lib/material.cpp
+++ lib/material.cpp
@@ -1,7 +1,54 @@
-#include "dielectric.hpp"
-#include "base.hpp"
+#include "material.hpp"
+#include <glm/gtc/random.hpp>
 
 using namespace rtc;
+
+Lambertian::Lambertian()
+{
+}
+
+Lambertian::Lambertian(const color &albedo) : albedo_(albedo)
+{
+}
+
+Lambertian::~Lambertian()
+{
+}
+
+bool Lambertian::scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const
+{
+    vec3 scatter_dir = rec.normal() + glm::ballRand(1.0);
+
+    if (almost_zero(scatter_dir)) // in case the scatter direction is zero, we ignore it
+        scatter_dir = rec.normal();
+
+    scattered = Ray(rec.point_, scatter_dir);
+    attenuation = albedo_;
+    return true;
+}
+
+Metal::Metal()
+{
+}
+
+Metal::Metal(const color &albedo, const double fuzz) : albedo_(albedo), fuzz_(fuzz < 1 ? fuzz : 1)
+{
+}
+
+Metal::~Metal()
+{
+}
+
+bool Metal::scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const
+{
+    vec3 reflected = glm::reflect(ray.dir_, rec.normal());
+    scattered = Ray(rec.point_, reflected + fuzz_ * glm::ballRand(1.0));
+    attenuation = albedo_;
+
+    // if this dot product is positive, then the reflected ray is indeed reflected (travelling away from surface), else
+    // it is absorbed
+    return glm::dot(reflected, rec.normal()) > 0;
+}
 
 Dielectric::Dielectric()
 {
--- lib/material.hpp
+++ lib/material.hpp
@@ -19,6 +19,51 @@ class Material
     virtual bool scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const = 0;
 };
 
+// a diffuse material
+class Lambertian : public Material
+{
+  public:
+    Lambertian();
+    Lambertian(const color &albedo);
+    virtual ~Lambertian() override;
+
+    virtual bool scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const override;
+
+  public:
+    color albedo_;
+};
+
+class Metal : public Material
+{
+  public:
+    Metal();
+    Metal(const color &albedo, double fuzz);
+    virtual ~Metal() override;
+
+    virtual bool scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const override;
+
+  public:
+    color albedo_;
+
+    // degree of randomness in direction of scattered ray, 0 for a mirror-like look and 1 for an almost diffuse look
+    double fuzz_;
+};
+
+class Dielectric : public Material
+{
+  public:
+    Dielectric();
+    Dielectric(double refractive_index);
+    virtual ~Dielectric() override;
+    virtual bool scatter(const Ray &ray, const HitRecord &rec, color &attenuation, Ray &scattered) const override;
+
+  private:
+    static double reflectance(double cosine, double ref_idx);
+
+  public:
+    double ri_; // the refractive index
+};
+
 } // namespace rtc
 
 #endif
